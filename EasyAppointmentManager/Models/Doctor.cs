05346f927cbc688d32438fcadc0e59f6b2e9d775
Campbell Rex <campbellrex@outlook.com>
2023-04-11 16:52:48 -0700

    Added forgoten documentation for class, added data annotation

--- EasyAppointmentManager/Models/Doctor.cs
+++ EasyAppointmentManager/Models/Doctor.cs
@@ -2,28 +2,36 @@
 
 namespace EasyAppointmentManager.Models
 {
+    /// <summary>
+    /// Represents a Doctor
+    /// </summary>
     public class Doctor
     {
         /// <summary>
         /// The Doctor's ID
         /// </summary>
         [Key]
-        public int ID { get; set; }
+        public int ID { get; private set; }
+
         /// <summary>
         /// The Doctor's legal first name
         /// </summary>
         [Required]
-        public string FirstName { get; set; }
+        [StringLength(55)]
+        public string? FirstName { get; set; }
+
         /// <summary>
         /// The Doctor's legal last name
         /// </summary>
         [Required]
-        public string LastName { get; set; }
+        [StringLength(55)]
+        public string? LastName { get; set; }
 
         /// <summary>
         /// The Doctor's legal middle name (Optional) 
         /// </summary>
-        public string MiddleName { get; set; }
+        [StringLength(55)]
+        public string? MiddleName { get; set; }
 
         /// <summary>
         /// The Doctor's legal date of birth
@@ -35,12 +43,12 @@ namespace EasyAppointmentManager.Models
         /// The Doctor's specialization represented by ID
         /// </summary>
         [Required]
-        public int SpecializationID { get; set; }
+        public int SpecializationID { get; private set; }
 
         /// <summary>
         /// The address of the location/building the doctors works for/in
         /// </summary>
         [Required]
-        public string PlaceOfWork { get; set; }
+        public string? PlaceOfWork { get; set; }
     }
 }
