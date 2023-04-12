7bb70b120d51da1d2364009890b53968079804ed
TUNG N KIM <Tungkims@gmail.com>
2023-04-11 19:01:56 -0500

    Created a class for Customers

---
+++ EasyAppointmentManager/Models/Customers.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyAppointmentManager.Models
+{
+    /// <summary>
+    /// Represents a single Customer/Patient
+    /// </summary>
+    public class Customers
+    {
+        /// <summary> 
+        /// The unique identifier for the customer
+        /// </summary>
+        [Key]
+        public int CustomerId { get; set; }
+
+        /// <summary>
+		/// The legal first name
+		/// </summary>
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+		/// The legal last name
+		/// </summary>
+		[Display(Name = "Last Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Customer's Date of birth
+        /// </summary>
+        [Display(Name = "Date Of Birth")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [DataType(DataType.Date)] // display Date picker
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gender of the customer
+        /// Female is True, of course :)
+        /// Male is False
+        /// Null is for Prefer not to answer
+        /// </summary>
+        public Boolean? Gender { get; set; }
+
+        /// <summary>
+        /// The customer's phone number
+        /// </summary>
+        [Required]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The customer's email address
+        /// </summary>
+        [StringLength(100)]
+        [Display(Name = "Student Email")]
+        // [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? Email { get; set; }
+    }
+}
