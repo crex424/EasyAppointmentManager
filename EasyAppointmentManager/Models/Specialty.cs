8c249c94fb158bc5840ad65b75c82cfe3816c3da
TUNG N KIM <Tungkims@gmail.com>
2023-04-11 22:10:01 -0500

    Created a class of Specialty

---
+++ EasyAppointmentManager/Models/Specialty.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyAppointmentManager.Models
+{
+    /// <summary>
+    /// Represents a single Specialty
+    /// </summary>
+    public class Specialty
+    {
+        /// <summary>
+        /// The unique identifier for the Specialty
+        /// </summary>
+        [Key]
+        public int SpecialtyId { get; set; }
+
+        /// <summary>
+        /// The name of the Specialty
+        /// </summary>
+        [Display(Name = "Name of Specialty")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the Specialty
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
