7965812be174886f5dd841da08a73216e9af8496
Campbell Rex <campbellrex@outlook.com>
2023-04-13 13:17:04 -0700

    Added documentation for fields

--- EasyAppointmentManager/Models/Location.cs
+++ EasyAppointmentManager/Models/Location.cs
@@ -7,21 +7,36 @@ namespace EasyAppointmentManager.Models
     /// </summary>
     public class Location
     {
+        /// <summary>
+        /// The Identity of the Location
+        /// </summary>
         [Key]
         public int LocationID { get; private set; }
 
+        /// <summary>
+        /// The Location's Address
+        /// </summary>
         [Display(Name = "Address Of Location")]
         [Required(ErrorMessage = "{0} is required.")]
         public string? Address { get; set; }
 
+        /// <summary>
+        /// The Location's City of residence
+        /// </summary>
         [Display(Name = "The Location's City")]
         [Required(ErrorMessage = "{0} is requried")]
         public string? City { get; set; }
 
+        /// <summary>
+        /// The Location's ZipCode
+        /// </summary>
         [Display(Name = "The Location's Zip Code")]
         [Required(ErrorMessage = "{0} is requried")]
         public string? ZipCode { get; set; }
 
+        /// <summary>
+        /// The Location's Name
+        /// </summary>
         [Display(Name = "Name Of Location")]
         [Required(ErrorMessage = "{0} is requried")]
         [StringLength(100)]
