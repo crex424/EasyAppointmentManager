44f6a3af82dc34cf46e1e1d14af40050f38a72ec
Campbell Rex <campbellrex@outlook.com>
2023-04-14 14:08:17 -0700

    Added documentation for fields

--- EasyAppointmentManager/Models/Fee.cs
+++ EasyAppointmentManager/Models/Fee.cs
@@ -7,17 +7,26 @@ namespace EasyAppointmentManager.Models
     /// </summary>
     public class Fee
     {
+        /// <summary>
+        /// The Fee's unique Identity
+        /// </summary>
         [Key]
         public int FeeID { get; private set; }
 
+        /// <summary>
+        /// The Fee's price
+        /// </summary>
         [Display(Name = "Price")]
         [Required(ErrorMessage = "{0} is requried.")]
         [DataType(DataType.Currency)]
         public double Price { get; set; }
 
+        /// <summary>
+        ///  The Fee's Name
+        /// </summary>
         [Display(Name = "The Fee's Name")]
         [Required(ErrorMessage = "{0} is requried.")]
         [StringLength(100)]
-        public string FeeName { get; set; }
+        public string? FeeName { get; set; }
     }
 }
