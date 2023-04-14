14e89cc90896d152149f8eba2520cc1fe57d4d7e
Campbell Rex <campbellrex@outlook.com>
2023-04-14 14:07:03 -0700

    Added data annotations for fields

--- EasyAppointmentManager/Models/Fee.cs
+++ EasyAppointmentManager/Models/Fee.cs
@@ -1,17 +1,23 @@
-﻿namespace EasyAppointmentManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyAppointmentManager.Models
 {
     /// <summary>
     /// Represents a Fee
     /// </summary>
     public class Fee
     {
-        
+        [Key]
         public int FeeID { get; private set; }
 
-
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "{0} is requried.")]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
 
-
+        [Display(Name = "The Fee's Name")]
+        [Required(ErrorMessage = "{0} is requried.")]
+        [StringLength(100)]
         public string FeeName { get; set; }
     }
 }
