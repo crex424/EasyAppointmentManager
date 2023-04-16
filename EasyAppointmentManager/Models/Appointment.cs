08f90e548ee57ddb04eed51476e8720cf9d4731c
TungKimProgrammer <tungkims@gmail.com>
2023-04-15 23:39:48 -0500

    Added properties to Appointment class

--- EasyAppointmentManager/Models/Appointment.cs
+++ EasyAppointmentManager/Models/Appointment.cs
@@ -13,11 +13,30 @@ namespace EasyAppointmentManager.Models
         [Key]
         public int AppointmentId { get; set; }
 
+        /// <summary>
+        /// Date of the appointment
+        /// </summary>
+        [Display(Name = "Appointment Date")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [DataType(DataType.Date)] // display Date picker
         public DateTime Date { get; set; }
 
+        /// <summary>
+        /// Timeslot of the appointment
+        /// Assume each visit is 1 hour
+        /// Every work day has 8 business hours divided into 8 timeslots
+        /// </summary>
         public int Timeslot { get; set; }
 
-        public bool IsAvailable { get; set; }
+        // public AppointmentStatus  { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public Location Location { get; set; }
+
+        // public Service Service { get; set; }
+
+        public Doctor Doctor { get; set; }
 
 
     }
