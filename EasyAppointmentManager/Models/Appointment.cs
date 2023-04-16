32d949b87f53c672673f600c1888e16ea9aa15cb
TungKimProgrammer <tungkims@gmail.com>
2023-04-15 19:41:20 -0500

    Initiated Appointment class

---
+++ EasyAppointmentManager/Models/Appointment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyAppointmentManager.Models
+{
+    /// <summary>
+    /// Represents a single Appointment
+    /// </summary>
+    public class Appointment
+    {
+        /// <summary> 
+        /// The unique identifier for the appointment
+        /// </summary>
+        [Key]
+        public int AppointmentId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Timeslot { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+
+    }
+}
