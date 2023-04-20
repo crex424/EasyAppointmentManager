d849a7d8ba9d28f9528c8f11ffec0bfef3878eae
Campbell Rex <campbellrex@outlook.com>
2023-04-20 14:35:35 -0700

    Created Doctor Controller

---
+++ EasyAppointmentManager/Controllers/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyAppointmentManager.Controllers
+{
+    public class Doctor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
