dd8b367522883d68fd61afce394116d0e4361919
Campbell Rex <campbellrex@outlook.com>
2023-04-20 14:41:23 -0700

    Added Connection to dbcontext to controller as well as index and details method

--- EasyAppointmentManager/Controllers/Doctor.cs
+++ EasyAppointmentManager/Controllers/Doctor.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyAppointmentManager.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EasyAppointmentManager.Models;
 
 namespace EasyAppointmentManager.Controllers
 {
     public class Doctor : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DoctorController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            return _context.Doctor != null ?
+                        View(await _context.Doctor.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Customer'  is null.");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Doctor == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctor
+                .FirstOrDefaultAsync(m => m.DoctorID == id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
     }
 }
