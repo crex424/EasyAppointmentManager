6b173d6a599540f6e63abd152a956026d5ca9533
Campbell Rex <campbellrex@outlook.com>
2023-04-21 14:11:57 -0700

    Added documentation and Create method for Doctor object

--- EasyAppointmentManager/Controllers/Doctor.cs
+++ EasyAppointmentManager/Controllers/Doctor.cs
@@ -5,7 +5,7 @@ using EasyAppointmentManager.Models;
 
 namespace EasyAppointmentManager.Controllers
 {
-    public class Doctor : Controller
+    public class DoctorController : Controller
     {
         private readonly ApplicationDbContext _context;
 
@@ -13,14 +13,21 @@ namespace EasyAppointmentManager.Controllers
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Get Doctors
+        /// </summary>
+        /// <returns>Docotrs</returns>
         public async Task<IActionResult> Index()
         {
             return _context.Doctor != null ?
                         View(await _context.Doctor.ToListAsync()) :
                         Problem("Entity set 'ApplicationDbContext.Customer'  is null.");
         }
-
+        /// <summary>
+        /// Gets details of a Doctor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>The details of a Doctor</returns>
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Doctor == null)
@@ -35,6 +42,34 @@ namespace EasyAppointmentManager.Controllers
                 return NotFound();
             }
 
+            return View(doctor);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Creates a Doctor object
+        /// </summary>
+        /// <param name="doctor">The doctor to be created</param>
+        /// <returns>The new Doctor object</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DoctorID,FirstName,MiddleName,LastName,DateOfBirth" +
+                                                      ",Gender,SpecializationID,Email,PhoneNumber,PlaceOfWork")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(doctor);
+                await _context.SaveChangesAsync();
+
+                // Show success message on page
+                ViewData["Message"] = $"{doctor.LastName}, {doctor.FirstName} was added successfully!";
+
+                return View();
+            }
             return View(doctor);
         }
 
