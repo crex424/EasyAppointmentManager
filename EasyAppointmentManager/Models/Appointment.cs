bb4e20882f299508f8610d58fbb67845dc6eb102
TungKimProgrammer <tungkims@gmail.com>
2023-04-22 10:12:06 -0500

    Added All available Customer, Clinic, Service, Doctor

--- EasyAppointmentManager/Models/Appointment.cs
+++ EasyAppointmentManager/Models/Appointment.cs
@@ -19,26 +19,35 @@ namespace EasyAppointmentManager.Models
         [Display(Name = "Appointment Date")]
         [Required(ErrorMessage = "{0} is required.")]
         [DataType(DataType.Date)] // display Date picker
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         /// <summary>
+        /// Range from 0 to 23
         /// Timeslot of the appointment
         /// Assume each visit is 1 hour
-        /// Every work day has 8 business hours divided into 8 timeslots
+        /// 8 represents 8:00AM - 9:00AM
         /// </summary>
+        [Range(0, 23)]
+        [Display(Name = "Time")]
+        [Required(ErrorMessage = "{0} is required.")]
         public int Timeslot { get; set; }
 
+        [Display(Name = "Timeslot Status")]
+        [Required(ErrorMessage = "{0} is required.")]
         public bool TimeslotStatus { get; set; }
 
+        [Display(Name = "Appointment Status")]
+        [Required(ErrorMessage = "{0} is required.")]
         public AppointmentStatus AppointmentStatus { get; set; }
 
-        public Customer Customer { get; set; }
+        public Customer? Customer { get; set; }
 
-        public Clinic Clinic { get; set; }
+        public Clinic? Clinic { get; set; }
 
-        public Service Service { get; set; }
+        public Service? Service { get; set; }
 
-        public Doctor Doctor { get; set; }
+        public Doctor? Doctor { get; set; }
 
 
     }
@@ -51,6 +60,7 @@ namespace EasyAppointmentManager.Models
         [Display(Name = "Appointment Date")]
         [Required(ErrorMessage = "{0} is required.")]
         [DataType(DataType.Date)] // display Date picker
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         /// <summary>
@@ -60,19 +70,33 @@ namespace EasyAppointmentManager.Models
         /// 8 represents 8:00AM - 9:00AM
         /// </summary>
         [Range(0, 23)]
+        [Display(Name = "Time")]
+        [Required(ErrorMessage = "{0} is required.")]
         public int Timeslot { get; set; }
 
+        [Display(Name = "Status")]
+        [Required(ErrorMessage = "{0} is required.")]
         public bool TimeslotStatus { get; set; }
 
+        [Display(Name = "Status")]
+        [Required(ErrorMessage = "{0} is required.")]
         public AppointmentStatus AppointmentStatus { get; set; }
 
-        public List<Customer> AllAvailableCustomers { get; set; }
+        public List<Customer>? AllAvailableCustomers { get; set; }
 
-        public List<Clinic> AllAvailableClinics { get; set; }
+        public Customer ChosenCustomer { get; set; }
 
-        public List<Service> AllAvailableServices { get; set; }
+        public List<Clinic>? AllAvailableClinics { get; set; }
 
-        public List<Doctor> AllAvailableDoctors { get; set; }
+        public Clinic ChosenClinic { get; set; }
+
+        public List<Service>? AllAvailableServices { get; set; }
+
+        public Service ChosenService { get; set; }
+
+        public List<Doctor>? AllAvailableDoctors { get; set; }
+
+        public Doctor ChosenDoctor { get; set; }
     }
 
     public enum AppointmentStatus
