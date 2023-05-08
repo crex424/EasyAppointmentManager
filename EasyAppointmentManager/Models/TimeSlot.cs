f4b1f2f54f5bc95cf01c13bf955b242dd2100ba1
TungKimProgrammer <tungkims@gmail.com>
2023-05-07 23:58:49 -0500

    Revert "Added Code to populate TimeSlot data"
    This reverts commit 54bc0d97b8eafa73ef084d6242c410a60c450f4b.

--- EasyAppointmentManager/Models/TimeSlot.cs
+++ EasyAppointmentManager/Models/TimeSlot.cs
@@ -1,5 +1,4 @@
-﻿using EasyAppointmentManager.Data;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
 namespace EasyAppointmentManager.Models
@@ -99,58 +98,4 @@ namespace EasyAppointmentManager.Models
         [Display(Name = "Doctors")]
         public string DoctorName { get; set; }
     }
-
-    // Populate database with TimeSlot records with the TimeSlotStatus set to "Available" in ASP.NET
-    // 1. Create a database seed method in application. This method will be responsible for adding initial data to the database.
-    public static class DatabaseSeeder
-    {
-        public static void SeedData(ApplicationDbContext context)
-        {
-            if (context.TimeSlot.Any())
-            {
-                return; // Database already seeded, no action needed
-            }
-
-            var doctors = context.Doctor.ToList();
-
-            var timeSlots = new List<TimeSlot>();
-
-            var startTime = new TimeSpan(8, 0, 0); // Start time: 8:00 AM
-            var endTime = new TimeSpan(16, 0, 0); // End time: 4:00 PM
-
-            var currentTime = startTime;
-
-            while (currentTime < endTime)
-            {
-                foreach (var doctor in doctors)
-                {
-                    var timeSlot = new TimeSlot
-                    {
-                        TimeSlotDate = DateTime.Now.Date, // Set the date as desired
-                        StartTime = currentTime,
-                        EndTime = currentTime.Add(new TimeSpan(1, 0, 0)), // Set the duration to one hour
-                        TimeSlotStatus = TimeslotStatus.Available,
-                        DoctorId = doctor.DoctorId
-                    };
-
-                    timeSlots.Add(timeSlot);
-                }
-
-                currentTime = currentTime.Add(new TimeSpan(1, 0, 0)); // Increment current time by one hour
-            }
-
-            context.TimeSlot.AddRange(timeSlots);
-            context.SaveChanges();
-        }
-        // Also add below code to Program.cs
-        /* 
-        // Create a scope to resolve the database context and call the seed method
-        using (var scope = serviceProvider.ServiceProvider.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            DatabaseSeeder.SeedData(dbContext);
-        }
-        */
-    }
-
 }
