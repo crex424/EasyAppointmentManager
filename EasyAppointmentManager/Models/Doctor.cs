1cbe25f1148c41b808e1e8949c5910ccc5ab23d9
Campbell Rex <campbellrex@outlook.com>
2023-05-08 17:49:15 -0700

    Added all needed fields for Doctor in addition to a list of specialties

--- EasyAppointmentManager/Models/Doctor.cs
+++ EasyAppointmentManager/Models/Doctor.cs
@@ -96,6 +96,69 @@ namespace EasyAppointmentManager.Models
     /// </summary>
     public class DoctorCreateViewModel
     {
+        /// <summary>
+        /// The Doctor's legal first name
+        /// </summary>
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100)]
+        public string? FirstName { get; set; }
 
+        /// <summary>
+        /// The Doctor's legal middle name (Optional) 
+        /// </summary>
+        [Display(Name = "Middle Name")]
+        [StringLength(55)]
+        public string? MiddleName { get; set; }
+
+        /// <summary>
+        /// The Doctor's legal last name
+        /// </summary>
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// The Doctor's legal date of birth
+        /// </summary>
+        [Display(Name = "Date Of Birth")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The legal gender of the Doctor
+        /// Female = true
+        /// Male = false
+        /// Other/Prefer not to answer = null
+        /// </summary>
+        [Display(Name = "Gender")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public bool? Gender { get; set; }
+
+        /// <summary>
+        /// Grabs a list of all specialties to choose from
+        /// </summary>
+        [Display(Name = "Specialty")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public List<Specialty> Specialties { get; set; }
+
+        /// <summary>
+        /// The Doctor's email address
+        /// </summary>
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100)]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// The Doctors Phone Number
+        /// </summary>
+        [Display(Name = "Phone Number")]
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; }
     }
 }
