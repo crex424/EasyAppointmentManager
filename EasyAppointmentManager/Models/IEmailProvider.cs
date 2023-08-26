73fbd1cf4eb0328cf27f979b71ff383928e1d0a8
TungKimProgrammer <tungkims@gmail.com>
2023-08-26 15:17:16 -0500

    Added SendGrid to HomeController

---
+++ EasyAppointmentManager/Models/IEmailProvider.cs
@@ -0,0 +1,36 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace EasyAppointmentManager.Models
+{
+    public interface IEmailProvider
+    {
+        Task SendEmailAsync(string toEmail, string fromEmail, string subject, string message, string htmlMessage);
+
+
+    }
+
+    public class EmailProviderSendGrid: IEmailProvider
+    {
+        private readonly IConfiguration _config;
+        public EmailProviderSendGrid(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string fromEmail, string subject, string message, string htmlMessage)
+        {
+            var apiKey = _config.GetSection("SendGridKey").Value;
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_config.GetSection("FromEmail").Value, "EasyAppointmentManager-DevTeam"),
+                Subject = "News from EasyAppointmentManager-DevTeam",
+                PlainTextContent = "with the latest updates from our developers team",
+                HtmlContent = "<strong>with the latest updates from our developers team</strong>"
+            };
+            msg.AddTo(new EmailAddress(_config.GetSection("FromEmail").Value, "EasyAppointmentManager-DevTeam"));
+            // var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+    }
+}
--- EasyAppointmentManager/Program.cs
+++ EasyAppointmentManager/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-
+builder.Services.AddTransient<IEmailProvider, EmailProviderSendGrid>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
