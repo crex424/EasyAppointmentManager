7191c530fe0c15ea0ab02acced0430e91b78aeb1
Campbell Rex <campbellrex@outlook.com>
2023-08-31 13:58:41 -0700

    Added documentation for fields and constructors in the CustomerCatalogViewModel

--- EasyAppointmentManager/Models/CustomerCatalogViewModel.cs
+++ EasyAppointmentManager/Models/CustomerCatalogViewModel.cs
@@ -1,14 +1,25 @@
 ﻿namespace EasyAppointmentManager.Models
 {
+    /// <summary>
+    /// A pagenation helper class
+    /// </summary>
     public class CustomerCatalogViewModel
     {
+        /// <summary>
+        /// Constructor method for CustomerCatalog
+        /// </summary>
+        /// <param name="customers">A list of customers</param>
+        /// <param name="lastPage">The last page of the catalog</param>
+        /// <param name="currPage">The current page the user is viewing</param>
         public CustomerCatalogViewModel(List<Customer> customers, int lastPage, int currPage)
         {
             Customers = customers;
             LastPage = lastPage;
             CurrentPage = currPage;
         }
-
+        /// <summary>
+        /// List of customers to be shown in the catalog
+        /// </summary>
         public List<Customer> Customers { get; private set; }
 
         /// <summary>
